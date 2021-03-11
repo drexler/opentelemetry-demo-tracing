9d47e99c92d4cacb3f4676d8e7f47401c9200c4a
drexler <andrew.quartey@gmail.com>
2021-03-10 22:34:05 -0500

    chore: skeleton ach-processor complete

--- services/ach-processor/ach-processor.ts
+++ services/ach-processor/ach-processor.ts
@@ -1,6 +1,24 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader';
+
+const PROTO_PATH = `${__dirname}/protos/hello.proto`; 
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+
+const packageObject: any = grpc.loadPackageDefinition(packageDefinition).helloworld
+const hello_client = new packageObject.Greeter(
+    "grpc-server:50051", 
+    grpc.credentials.createInsecure()
+);
+
 
 const port = process.env.SERVER_PORT || 3050;
 
@@ -16,6 +34,14 @@ app.get('/:test', (req: Request, res: Response) => {
     const param = req.params.test;
     res.status(200).send('hi from ACH- Get');
 });
+
+app.get('/grpc/:name', (request: Request, response: Response) => {
+    const user = request.params.name;
+    console.log('calling into gRPC service'); 
+    hello_client.sayHello({name: user}, (err: any, result: any) => {
+        response.send(result.message);
+    })
+})
 
 // start the express server
 const server = app.listen( port, () => {
