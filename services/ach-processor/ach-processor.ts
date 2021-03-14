ad4f1ccd87884bf5cdddc2ab530de06eecc2efe1
drexler <andrew.quartey@gmail.com>
2021-03-14 18:33:57 -0400

    chore: remove placeholder gRPC service

--- services/ach-processor/ach-processor.ts
+++ services/ach-processor/ach-processor.ts
@@ -4,9 +4,7 @@ import cors from 'cors';
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader';
 
-const HELLO_PROTO_PATH = `${__dirname}/protos/hello.proto`; 
 const protoOptions = {
-
     keepCase: true,
     longs: String,
     enums: String,
@@ -14,19 +12,9 @@ const protoOptions = {
     oneofs: true,
 };
 
-const helloPackageDefinition = protoLoader.loadSync(HELLO_PROTO_PATH, protoOptions);
-
-const helloPackageObject: any = grpc.loadPackageDefinition(helloPackageDefinition).helloworld
-const hello_client = new helloPackageObject.Greeter(
-    "grpc-server:50051", 
-    grpc.credentials.createInsecure()
-);
-
-
 const EMPLOYEE_PROTO_PATH = `${__dirname}/protos/employee.proto`; 
 
 const employeePackageDefinition = protoLoader.loadSync(EMPLOYEE_PROTO_PATH, protoOptions);
-
 const employeePackageObject: any = grpc.loadPackageDefinition(employeePackageDefinition).employee
 const employee_client = new employeePackageObject.EmployeeService(
     "employee-service:50052", 
@@ -34,29 +22,10 @@ const employee_client = new employeePackageObject.EmployeeService(
 );
 
 
-
 const port = process.env.SERVER_PORT || 3050;
 
 const app = express();
 app.use(cors());
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('hi from ACH');
-});
-
-
-// app.get('/:test', (req: Request, res: Response) => {
-//     const param = req.params.test;
-//     res.status(200).send('hi from ACH- Get');
-// });
-
-app.get('/grpc/:name', (request: Request, response: Response) => {
-    const user = request.params.name;
-    console.log('calling into gRPC service'); 
-    hello_client.sayHello({name: user}, (err: any, result: any) => {
-        response.send(result.message);
-    })
-})
 
 
 app.get('/employee-service', (_request: Request, response: Response) => {
@@ -70,4 +39,4 @@ app.get('/employee-service', (_request: Request, response: Response) => {
 const server = app.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
-} );
+});
