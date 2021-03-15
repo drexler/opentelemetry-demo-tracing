4c9308356d92002240b0e6eabdaefc5c93ba9cb0
drexler <andrew.quartey@gmail.com>
2021-03-15 17:57:01 -0400

    refactor: use release candidate libraries & instrumentation

--- services/ach-processor/tracing.ts
+++ services/ach-processor/tracing.ts
@@ -1,41 +1,33 @@
 import { NodeTracerProvider } from '@opentelemetry/node';
-import { SimpleSpanProcessor, BatchSpanProcessor } from '@opentelemetry/tracing';
+import { BatchSpanProcessor } from '@opentelemetry/tracing';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 import { registerInstrumentations } from '@opentelemetry/instrumentation'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+import { GrpcInstrumentation } from '@opentelemetry/instrumentation-grpc'
 
 const SERVICE_NAME: string = 'ach-processor'
 
-
 const provider = new NodeTracerProvider();
 registerInstrumentations({
+  tracerProvider: provider,
   instrumentations: [
+    new HttpInstrumentation({}),
+    new GrpcInstrumentation({}),
     {
-      plugins: {
-        https: {
-          enabled: true, 
-          path: '@opentelemetry/plugin-https'
-        }, 
-        http: {
-          enabled: true,
-          path: '@opentelemetry/plugin-http',
-        },
-        express: {
-            enabled: true,
-            path: '@opentelemetry/plugin-express'
-        },
-        '@grpc/grpc-js': {
-            enabled: false,  //TODO: known issue: https://github.com/open-telemetry/opentelemetry-js/issues/1705
-            path: '@opentelemetry/plugin-grpc-js',
-          }
-      }
+        plugins: {
+            http: { enabled: false, path: '@opentelemetry/plugin-http' },
+            https: { enabled: false, path: '@opentelemetry/plugin-https' },
+            '@grpc/grpc-js': { enabled: false, path: '@opentelemetry/plugin-grpc-js' },
+        }
     }
   ],
-  tracerProvider: provider,
 });
-  
-const exporter = new CollectorTraceExporter({ serviceName: SERVICE_NAME, url: 'http://otel-collector:55681/v1/trace' });
-provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
+const exporter = new CollectorTraceExporter({ 
+    serviceName: SERVICE_NAME, 
+    url: 'http://otel-collector:55681/v1/trace' 
+});
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 provider.register();
 
 console.log('Tracing Initialized');
