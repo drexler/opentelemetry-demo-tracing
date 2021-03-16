675e0e54e3c949158ac065c93e2c031f346bbfa1
drexler <andrew.quartey@gmail.com>
2021-03-15 22:52:08 -0400

    feat: add tracing instrumentation to employee service

--- services/employee/server.rs
+++ services/employee/server.rs
@@ -1,18 +1,55 @@
 use employee::employee_service_server::{EmployeeService, EmployeeServiceServer};
 use employee::{Employee, GetAllEmployeesResponse, GetEmployeeRequest, GetEmployeeResponse};
+use opentelemetry::global;
+use opentelemetry::sdk::propagation::TraceContextPropagator;
+use opentelemetry::sdk::{trace, Resource};
+use opentelemetry::trace::TraceError;
+use opentelemetry::{
+    propagation::Extractor,
+    trace::{Span, Tracer},
+    KeyValue,
+};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod employee {
     tonic::include_proto!("employee");
 }
 
-// defining a struct for our service
+struct MetadataMap<'a>(&'a tonic::metadata::MetadataMap);
+
+impl<'a> Extractor for MetadataMap<'a> {
+    /// Get a value for a key from the MetadataMap.  If the value can't be converted to &str, returns None
+    fn get(&self, key: &str) -> Option<&str> {
+        self.0.get(key).and_then(|metadata| metadata.to_str().ok())
+    }
+
+    /// Collect all the keys from the MetadataMap.
+    fn keys(&self) -> Vec<&str> {
+        self.0
+            .keys()
+            .map(|key| match key {
+                tonic::metadata::KeyRef::Ascii(v) => v.as_str(),
+                tonic::metadata::KeyRef::Binary(v) => v.as_str(),
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
 #[derive(Default)]
 pub struct MyEmployeeService {}
 
 #[tonic::async_trait]
 impl EmployeeService for MyEmployeeService {
-    async fn get_all_employees(&self, _request: Request<()>) -> Result<Response<GetAllEmployeesResponse>, Status> {
+    async fn get_all_employees(
+        &self,
+        request: Request<()>,
+    ) -> Result<Response<GetAllEmployeesResponse>, Status> {
+        let parent_ctx =
+            global::get_text_map_propagator(|prop| prop.extract(&MetadataMap(request.metadata())));
+        let span = global::tracer("employee-service-tracer")
+            .start_with_context("get_all_employees", parent_ctx);
+        span.set_attribute(KeyValue::new("request", format!("{:?}", request)));
+
         let employee = Employee {
             id: "Test Andrew".into(),
             name: "Name".into(),
@@ -40,19 +77,31 @@ impl EmployeeService for MyEmployeeService {
             marital_status: "M".into(),
         };
 
-        let result = GetEmployeeResponse { employee: Some(employee) };
+        let result = GetEmployeeResponse {
+            employee: Some(employee),
+        };
 
         Ok(Response::new(result))
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let address = "[::0]:50052".parse().unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+    let (_tracer, _uninstall) = opentelemetry_otlp::new_pipeline()
+        .with_endpoint("http://[::0]:4317") //scheme needed
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                "service.name",
+                "employee-service",
+            )])),
+        )
+        .install()?;
 
+    let address = "[::0]:50052".parse().unwrap();
     let employee_service = MyEmployeeService::default();
     println!("Server listening on {}", address);
-    // adding our service to our server.
+
     Server::builder()
         .add_service(EmployeeServiceServer::new(employee_service))
         .serve(address)
